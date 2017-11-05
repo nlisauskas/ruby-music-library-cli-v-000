90bea8e91f95d6eb4aafbf964523cb5d38d871de
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-11-05 15:22:26 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -40,8 +40,9 @@ class MusicLibraryController
 
   def list_songs
     i = 0
-    while i < Song.all.length
-    Song.all.each do |song|
+    all = Song.all.sort
+    while i < all.length
+    all.each do |song|
       puts "#{i + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
       i +=1
     end
